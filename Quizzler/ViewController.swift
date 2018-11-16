4ff635d6ea23ea46b8dc1e10fc361e81f9951fb8
Tprezioso <tommyprezioso@gmail.com>
2018-11-16 17:52:44 -0500

    got basic functionality of quiz app to work

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,11 +23,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
   
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+
+        checkAnswer()
+        questionNumber += 1
+        nextQuestion()
     }
     
     
@@ -35,19 +49,37 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+        if questionNumber <= allQuestions.list.count - 1  {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        } else {
+            let alert = UIAlertController(title: "AWESOME", message: "You have finished all the question, do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
+
     }
     
     
     func checkAnswer() {
-        
-    }
     
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("corret!")
+        } else {
+            print("wrong!")
+        }
+    
+    }
     
     func startOver() {
-       
+        questionNumber = 0
+        nextQuestion()
     }
-    
-
     
 }
